25b33703be6eda35c2023567db34aaf5defe984b
Barry Carlyon | Kora <barry@barrycarlyon.co.uk>
2020-07-27 17:53:35 +0100

    How to get both the dumb iframe and everywhere Embed's working in Electron

---
+++ player/electron/README.md
@@ -0,0 +1,19 @@
+## What is this example
+
+This example covers a methjod to get Electron to "behave" for Embedding streams.
+
+It supports both "dumb iframe" embeds and "Everywhere" embeds
+
+This example also includes the player "test" controls used form the ["Web" Example](https://twitch.extensions.barrycarlyon.co.uk/temp/player_test.html)
+
+## Reference Documentation
+
+- [Embed Everything](https://dev.twitch.tv/docs/embed/everything)
+- ["Dumb" iFrames](https://dev.twitch.tv/docs/embed/video-and-clips)
+
+## Running the example
+
+In a console/terminal, run these commands:
+
+- `npm install`
+- `npm start`
---
+++ player/electron/app.js
@@ -0,0 +1,65 @@
+const { app, BrowserWindow, session } = require('electron')
+
+function createWindow () {
+
+  session.defaultSession.webRequest.onBeforeRequest({
+    urls: [
+      'https://embed.twitch.tv/*channel=*'
+    ]
+  }, (details, cb) => {
+    var redirectURL = details.url;
+
+    var params = new URLSearchParams(redirectURL.replace('https://embed.twitch.tv/',''));
+    if (params.get('parent') != '') {
+        cb({});
+        return;
+    }
+    params.set('parent', 'locahost');
+    params.set('referrer', 'https://localhost/');
+
+    var redirectURL = 'https://embed.twitch.tv/?' + params.toString();
+    console.log('Adjust to', redirectURL);
+
+    cb({
+      cancel: false,
+      redirectURL
+    });
+  });
+
+  // works for dumb iFrames
+  session.defaultSession.webRequest.onHeadersReceived({
+    urls: [
+      'https://player.twitch.tv/*',
+      'https://embed.twitch.tv/*'
+    ]
+  }, (details, cb) => {
+    var responseHeaders = details.responseHeaders;
+
+    console.log('headers', details.url, responseHeaders);
+
+    delete responseHeaders['Content-Security-Policy'];
+    //console.log(responseHeaders);
+
+    cb({
+      cancel: false,
+      responseHeaders
+    });
+  });
+
+
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
